75642eee174666bf7dcdcbfd34fa87f1406ee719
Chronophylos <nikolai@chronophylos.com>
2019-11-14 17:10:19 +0100

    move some function into their own file
    Signed-off-by: Chronophylos <nikolai@chronophylos.com>

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
-	"regexp"
 	dbg "runtime/debug"
 	"strconv"
 	"strings"
@@ -920,42 +919,6 @@ func getWeather(city string) string {
 // }}}
 
 // Helper Functions {{{
-func rl(re ...string) []*regexp.Regexp {
-	res := []*regexp.Regexp{}
-
-	for _, r := range re {
-		res = append(res, regexp.MustCompile(r))
-	}
-
-	return res
-}
-
-func censor(text string) string {
-	if *showSecrets {
-		return text
-	}
-	return "[REDACTED]"
-}
-
-func pluralize(text string, times int64) string {
-	if times > 1 {
-		return strconv.FormatInt(times, 10) + " " + text + "s"
-	}
-	return "one " + text
-}
-
-func join(log zerolog.Logger, channel string) {
-	twitchClient.Join(channel)
-	stateClient.JoinChannel(channel, true)
-	log.Info().Str("channel", channel).Msg("Joined new channel")
-}
-
-func part(log zerolog.Logger, channel string) {
-	twitchClient.Depart(channel)
-	stateClient.JoinChannel(channel, false)
-	log.Info().Str("channel", channel).Msg("Parted from channel")
-}
-
 func setGlobalLogger() {
 	level := zerolog.InfoLevel
 
@@ -993,27 +956,6 @@ func setGlobalLogger() {
 		output := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.StampMilli}
 		log.Logger = log.Output(output)
 	}
-}
-
-func checkIfBotname(name string) bool {
-	switch name {
-	case "nightbot":
-		fallthrough
-	case "fossabot":
-		fallthrough
-	case "streamelements":
-		return true
-	}
-	return false
-}
-
-// Credit: https://stackoverflow.com/users/130095/geoff
-func truncate(s string, i int) string {
-	runes := []rune(s)
-	if len(runes) > i {
-		return string(runes[:i])
-	}
-	return s
 }
 
 // }}}
