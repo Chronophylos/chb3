2fd0f9d2f92d14e832a8ccb271e2aec11ef1bae6
Chronophylos <nikolai@chronophylos.com>
2020-02-01 16:01:18 +0100

    removed unused error
    Signed-off-by: Chronophylos <nikolai@chronophylos.com>

--- cmd/actions/errors.go
+++ cmd/actions/errors.go
@@ -2,18 +2,6 @@ package actions
 
 import "fmt"
 
-type noPermissionError struct {
-	has    Permission
-	needed Permission
-}
-
-func (e *noPermissionError) Error() string {
-	return fmt.Sprintf(
-		"needed permission is not high enough (has: %d, needed: %d)",
-		e.has, e.needed,
-	)
-}
-
 type notInBotChannelError struct {
 	channel string
 }
